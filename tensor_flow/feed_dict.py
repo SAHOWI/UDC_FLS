c40537f4e5f5d03b10fb7ffa7a25739765788203
SAHOWI <wienecke.ch@gmail.com>
2019-10-17 00:38:22 +0200

    added first TF scripts

---
+++ tensor_flow/feed_dict.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from grader import get_result
+
+import tensorflow as tf
+
+def run():
+    output = None
+    x = tf.placeholder(tf.int32)
+
+    with tf.Session() as sess:
+        output = sess.run(x, feed_dict={x: '123'})
+
+    return output
+
+### DON'T MODIFY ANYTHING BELOW ###
+### Be sure to run all cells above before running this cell ###
+import grader
+
+try:
+    grader.get_result(run)
+except Exception as err:
+    print(str(err))

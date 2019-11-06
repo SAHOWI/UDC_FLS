57689be758f18b4de8f3124c3957bd64003ff7fa
SAHOWI <wienecke.ch@gmail.com>
2019-11-06 20:57:16 +0100

    added pooling

--- keras_model_snip.py
+++ keras_model_snip.py
@@ -1,9 +1,12 @@
 # TODO: Build Convolutional Neural Network in Keras Here
+# added pooling
+
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=(32,32,3)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 
 model.add(Activation('relu'))
 model.add(Flatten())

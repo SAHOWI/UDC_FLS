099937a0d1cb1354c828247c3880ad25661ab443
SAHOWI <wienecke.ch@gmail.com>
2019-11-06 20:58:31 +0100

    added dropout

--- keras_model_snip.py
+++ keras_model_snip.py
@@ -1,13 +1,15 @@
 # TODO: Build Convolutional Neural Network in Keras Here
 # added pooling
+# added dropout
 
 
+# TODO: Build Convolutional Pooling Neural Network with Dropout in Keras Here
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=(32,32,3)))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
-
+model.add(Dropout(0.5, noise_shape=None, seed=None))
 model.add(Activation('relu'))
 model.add(Flatten())
 model.add(Dense(128))

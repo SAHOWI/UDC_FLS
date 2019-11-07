9a12a645ba70b2df56d636972c2cb1e2407e224e
SAHOWI <wienecke.ch@gmail.com>
2019-11-07 18:54:37 +0100

    adding logging/diagnostics

--- BehvClng/3cams-example.py
+++ BehvClng/3cams-example.py
@@ -31,3 +31,42 @@ import cv2
 model = Sequential()
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 ...
+
+
+
+
+### gathering logging information
+
+*** n Keras, the model.fit() and model.fit_generator() methods have a verbose parameter that tells Keras to output loss metrics as the model trains. The verbose parameter can optionally be set to verbose = 1 or verbose = 2.
+
+Setting model.fit(verbose = 1) will
+
+output a progress bar in the terminal as the model trains.
+output the loss metric on the training set as the model trains.
+output the loss on the training and validation sets after each epoch.
+With model.fit(verbose = 2), Keras will only output the loss on the training set and validation set after each epoch.
+*** 
+
+### showing logging information
+
+
+from keras.models import Model
+import matplotlib.pyplot as plt
+
+history_object = model.fit_generator(train_generator, samples_per_epoch =
+    len(train_samples), validation_data = 
+    validation_generator,
+    nb_val_samples = len(validation_samples), 
+    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
